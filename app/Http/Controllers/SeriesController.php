065b0a29c5c3a11f09549b04d97ae99a6e042688
Eduardo <coutoeduz@gmail.com>
2022-05-05 22:22:42 -0300

    Validando dados

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Serie;
 use Illuminate\Http\Request;
+use App\Http\Requests\SeriesFormRequest;
 
 class SeriesController extends Controller
 {
@@ -21,9 +22,9 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
-
+        
         $serie = Serie::create($request->all());
         $request->session()
             ->flash(
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -5,13 +5,24 @@ Adicionar série
 @endsection
 
 @section('conteudo')
-    <form method="post">
-        @csrf
-        <div class="form-group">
-            <label for="nome">Nome</label>
-            <input type="text" class="form-control" name="nome" id="nome">
 
-        </div>
-        <button class="btn btn-primary">Adicionar</button>
-    </form>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form method="post">
+    @csrf
+    <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" name="nome" id="nome">
+
+    </div>
+    <button class="btn btn-primary">Adicionar</button>
+</form>
 @endsection
